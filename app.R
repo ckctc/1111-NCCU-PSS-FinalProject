67f930ff5095868541f0fa87e00c1ff8b495849c
ckctc <93563987+ckctc@users.noreply.github.com>
2022-11-27 13:45:11 +0800

    Fundamental Structure
    *Just one chem input/output

---
+++ app.R
@@ -0,0 +1,46 @@
+##source
+source('helpers.R')
+
+##Test Web: https://www.cosdna.com/cht/cosmetic_7aa222320.html
+
+##UI
+ui <- fluidPage( #建立page
+  
+  fluidRow(
+    column(5,
+           textInput("web",h3("輸入保養品之cosDNA網址"), #建立文字Input框
+                     value = "Enter the Web Address ..."
+           )
+    )
+  ),
+  
+  fluidRow(
+    column(5,
+           DT::dataTableOutput("resultDT") #建立資料Output區域
+    )
+  )
+  
+)
+
+##Server
+server <- function(input, output) {
+  
+  dataInput <- reactive({ #處理dataInput #reactive用於當Input發生變動時的更新處理
+    
+    validate( #判斷Input是否符合要求
+      test.web(input$web)
+    )
+    
+    result <- get.chem(input$web)
+    
+    return(result)
+    
+  })
+  
+  output$resultDT <- DT::renderDataTable({#建立datatable輸出
+    dataInput()
+  })
+  
+}
+
+shinyApp(ui,server)
---
+++ helpers.R
@@ -0,0 +1,42 @@
+##引入library
+
+library(shiny) #UI
+library(DT) #DataTable
+library(rvest) #網頁擷取
+library(magrittr) #for chaining commands
+library(stringr) #字串處理
+library(bslib) #bootstrap sass
+
+##測試Input是否正確，且若不符則顯示警告訊息
+
+test.web <- function(input){
+  if (str_detect(input, pattern = "https://www.cosdna.com/cht/", negate = TRUE)){
+    "Please Enter a WebSite from cosDNA."
+  }else if (str_detect(input, pattern = ".html", negate = TRUE)){
+    "Please Enter a Completed Web Address."
+  }
+}
+
+##擷取網頁資料，並輸出成dataframe
+
+get.chem <- function(web){
+  
+  url <- as.character(web)
+  
+  htmlContent <- read_html(url)
+  
+  chemPath <- "/html/body/div[1]/main/div[1]/div[2]/div[1]/table/tbody/tr/td[1]/a/div[1]" #設定要擷取網頁中何處特定區域之資料（xpath）
+
+  chem <- htmlContent %>% html_nodes(xpath=chemPath) %>% html_text() #建立資料庫
+  
+  for (i in 1:length(chem)) { #處理資料
+    locate_end <- str_locate_all(chem[i],pattern = "\n")
+    names(locate_end) <- c("locate") #locate_end為list，將其命名以利索引
+    index_end <- locate_end$locate[3,1]
+    chem[i] <- str_sub(chem[i],start = 91, end = index_end-1)
+  }
+  
+  chem <- as.data.frame(chem) #將結果轉為dataframe
+  
+  
+}
